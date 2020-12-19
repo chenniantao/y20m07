8cb5e16a868d74f35a0f9c1e35b455df34491af4
TomChen <76876201@qq.com>
2020-12-19 08:01:16 +0800

    done

--- 04-project/mall-all/home/src/pages/common/side/index.js
+++ 04-project/mall-all/home/src/pages/common/side/index.js
@@ -9,10 +9,10 @@ var _util = require('util')
 var tpl = require('./index.tpl')
 
 var list = [
-    {name:'user-center',icon:'fa fa-user',desc:'用户中心',link:'./user-center.html'},
-    {name:'user-base',icon:'fa fa-cog',desc:'基本设置',link:'./user-base.html'},
-    {name:'order-list',icon:'fa fa-book',desc:'我的订单',link:'./order-list.html'},
-    {name:'user-update-password',icon:'fa fa-lock',desc:'修改密码',link:'./user-update-password.html'}
+    {name:'user-center',icon:'iconfont icon-user',desc:'用户中心',link:'./user-center.html'},
+    { name: 'user-base', icon:'iconfont icon-setting',desc:'基本设置',link:'./user-base.html'},
+    { name: 'order-list', icon:'iconfont icon-order',desc:'我的订单',link:'./order-list.html'},
+    { name: 'user-update-password', icon:'iconfont icon-lock',desc:'修改密码',link:'./user-update-password.html'}
 ]
 module.exports = {
     render:function(name){
